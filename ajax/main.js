9c954ff1bb850453f99d68ea1ebadc71790350e3
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-05-15 19:39:19 +0900

    try catch でerrorを返す

--- ajax/main.js
+++ ajax/main.js
@@ -1,6 +1,42 @@
 "use strict";
 
-const err = new Error();
-err.message = "erorrrr";
+// const err = new Error();
+// err.message = "erorrrr";
 
-throw err;
+// throw err;
+
+function UserException(message) {
+  this.message = message;
+  this.name = "UserException";
+}
+function getMonthName(mo) {
+  mo = mo - 1; // 配列の添字のために月の数を調整する (1 = Jan, 12 = Dec)
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  if (months[mo] !== undefined) {
+    return months[mo];
+  } else {
+    throw new UserException("InvalidMonthNo");
+  }
+}
+
+try {
+  // 試みる文
+  var myMonth = 15; // 15 は範囲外であり、例外が発生する
+  var monthName = getMonthName(myMonth);
+} catch (e) {
+  monthName = "unknown";
+  console.error(e.message, e.name); // エラーハンドラーに例外オブジェクトを渡す
+}
