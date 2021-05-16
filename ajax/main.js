78017c40b7a89c7fae55878ac22447d42a2e4261
yuuki_takahara <takahara.yuuki22@gmail.com>
2021-05-16 15:45:37 +0900

    fetch async/await

--- ajax/main.js
+++ ajax/main.js
@@ -1,1 +1,29 @@
 "use strinct";
+
+const url = "https://dog.ceo/api/breeds/image/random";
+
+const options = {
+  method: "GET",
+};
+
+const fetchTest = fetch(url, options).then((response) => response.json());
+// エラーが出る Promise{<pending>} 	pemding : 未決定
+console.log(fetchTest); // 値が入っていないうちにlogを表示してもundefined
+
+// サーバーからのデータの取得を待ってから処理をする
+// Promise ,async/await
+function getDogImage(url, options) {
+  return fetch(url, options).then((response) => response.json());
+}
+
+async function getImage(url, options) {
+  // getDogImageの処理が完了してresponseの中身が入ってからconsole.logをすることで値がundefinedにならない
+  const response = await getDogImage(url, options);
+  console.log(response.message);
+
+  const imageElement = document.createElement("img");
+  imageElement.src = response.message;
+  document.body.appendChild(imageElement);
+}
+
+getImage(url, options);
